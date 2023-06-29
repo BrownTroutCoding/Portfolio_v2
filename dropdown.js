cc753dfb49d91faf2f3258da91ecd3d656b3c8f7
BrownTroutCoding <browntroutcoding@gmail.com>
2023-06-29 10:58:11 -0600

    fixed dropdown not closing correctly

--- dropdown.js
+++ dropdown.js
@@ -1,49 +1,43 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-        // get all dropdowns from document
     const dropdowns = document.querySelectorAll('.dropdown');
 
-    // loop through all dropdown elements
     dropdowns.forEach(dropdown => {
-    // get inner elements from each dropdown
-    const select = dropdown.querySelector('.select');
-    const caret = dropdown.querySelector('.caret');
-    const menu = dropdown.querySelector('.menu');
-    const options = dropdown.querySelectorAll('.menu li');  // Changed to querySelectorAll
-    const selected = dropdown.querySelector('.selected');
+        const select = dropdown.querySelector('.select');
+        const caret = dropdown.querySelector('.caret');
+        const menu = dropdown.querySelector('.menu');
+        const options = dropdown.querySelectorAll('.menu li');
 
-    // add click event to the select element
-    select.addEventListener('click', () => {
-        // add the roate styles to caret
-        caret.classList.toggle('caret-rotate');
-        // add open styles to the menu element
-        menu.classList.toggle('menu-open');
-    });
+        select.addEventListener('click', (event) => {
+            event.stopPropagation();
+            caret.classList.toggle('caret-rotate');
+            menu.classList.toggle('menu-open');
+        });
 
-    // loop through all option elements
-    options.forEach(option => {
-        // add click event to option element
-        option.addEventListener('click', (event) => {
-        event.preventDefault(); // prevent the default action
-        // change selected inner text to clicked option inner text
-        selected.innerText = option.textContent;
-        // remove the rotate styles from caret
-        caret.classList.remove('caret-rotate');
-        // remove open styles from the menu element
-        menu.classList.remove('menu-open');
-        // remove active class from all option elements
         options.forEach(option => {
-            option.classList.remove('active');
-        });
-        // add active class to clicked option element
-        option.classList.add('active');
+            option.addEventListener('click', (event) => {
+                event.stopPropagation();
+                event.preventDefault();
+                caret.classList.remove('caret-rotate');
+                menu.classList.remove('menu-open');
+                
+                options.forEach(option => {
+                    option.classList.remove('active');
+                });
 
-        // scroll to the corresponding section
-        const targetSection = option.getAttribute('data-target');
-        const targetElement = document.querySelector(targetSection);
-        targetElement.scrollIntoView({ behavior: 'smooth' });
+                option.classList.add('active');
+
+                const targetSection = option.getAttribute('data-target');
+                const targetElement = document.querySelector(targetSection);
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            });
         });
-    });
+
+        // Listen for click events on the whole document
+        document.addEventListener('click', () => {
+            if (menu.classList.contains('menu-open')) {
+                caret.classList.remove('caret-rotate');
+                menu.classList.remove('menu-open');
+            }
+        });
     });
 });
-
-
